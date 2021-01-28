f8c2ef6b177c912252d1e1d1698b5f7c8c034ebc
Henrique Machiavelli <hsmachiavelli@gmail.com>
2021-01-28 16:47:31 -0300

    refactoring custom errors to middleware layer

--- index.php
+++ index.php
@@ -1,44 +1,15 @@
 <?php
 
-use Slim\Exception\HttpBadRequestException;
 use Slim\Factory\AppFactory;
-use Slim\Psr7\Response;
-use Slim\Exception\HttpNotFoundException;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
-// $app->setBasePath('/simple_balance_api');
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware = $app->addErrorMiddleware(false, true, true);
 
-$errorMiddleware->setErrorHandler(
-    HttpMethodNotAllowedException::class,
-    function () {
-        $response = new Response();
-        return $response->withStatus(405);
-    }
-);
-
-$errorMiddleware->setErrorHandler(
-    HttpNotFoundException::class,
-    function () {
-        $response = new Response();
-        $response->getBody()->write('0');
-        return $response->withStatus(404);
-    }
-);
-
-$errorMiddleware->setErrorHandler(
-    HttpBadRequestException::class,
-    function (\Psr\Http\Message\ServerRequestInterface $request, Throwable $exception) {
-        $response = new Response();
-        $response->getBody()->write($exception->getMessage());
-        return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
-    }
-);
-
+\App\Middleware\CustomErrorMiddleware::add($errorMiddleware);
 \Routes\ApiRouter::build($app);
 
 $app->run();
---
+++ src/Middleware/CustomErrorMiddleware.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Middleware;
+
+use Slim\Psr7\Response;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Exception\HttpBadRequestException;
+
+class CustomErrorMiddleware
+{
+    public static function add(\Slim\Middleware\ErrorMiddleware &$errorMiddleware)
+    {
+        $errorMiddleware->setErrorHandler(
+            HttpMethodNotAllowedException::class,
+            function () {
+                $response = new Response();
+                return $response->withStatus(405);
+            }
+        );
+
+        $errorMiddleware->setErrorHandler(
+            HttpNotFoundException::class,
+            function () {
+                $response = new Response();
+                $response->getBody()->write('0');
+                return $response->withStatus(404);
+            }
+        );
+
+        $errorMiddleware->setErrorHandler(
+            HttpBadRequestException::class,
+            function (\Psr\Http\Message\ServerRequestInterface $request, Throwable $exception) {
+                $response = new Response();
+                $response->getBody()->write($exception->getMessage());
+                return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+            }
+        );
+    }
+}
